0e89d240670ea2398ca5e16b0bbfc3a963e24d29
Azah237 <sheyrhonazah@gmail.com>
2026-09-16 13:34:01 +0100

    create impact project and impact record on admin dashboard

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import ImpactProject, ImpactRecord
+
+@admin.register(ImpactProject)
+class ImpactProjectAdmin(admin.ModelAdmin):
+    lsit_display =(
+        "name",
+        "country",
+        "location",
+        "impact_area",
+        "status",
+        "created_at",
+    )
+
+    list_filter =(
+        "impact_area",
+        "status",
+        "country",
+    )
+
+
+    search_fields=(
+        "name",
+        "description",
+        "country",
+        "location",
+    )
+
+    @admin.register(ImpactRecord)
+    class ImpactReecordAdmin(admin.ModelAdmin):
+        list_display=(
+            "project",
+            "metric",
+            "value",
+            "unit",
+            "recorded_at",
+        )
+
+        list_filter=(
+            "unit",
+            "recorded_at",
+        )
+
+        search_fields=(
+            "metric",
+            "project__name",
+        )
--- core/models.py
+++ core/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
 # Create your models here.
+
+class ImpactProject(models.Model):
+    IMPACT_AREAS=[
+        ('education', 'Education'),
+        ('health', 'Health'),
+        ('agriculture', 'Agriculture'),
+        ('environment', 'Environment'),
+        ('technology', 'Technology'),
+        ('employment', 'Employment'),
+        ('other', 'Other'),
+        ]
+    STATUS_CHOICES=[
+        ('planned', 'Planned'),
+        ('active', 'Active'),
+        ('completed', 'Completed'),
+    ]
+    name = models.CharField(max_length = 200)
+    description = models.TextField()
+
+
+    country = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
+
+
+    impact_area = models.CharField(max_length=50, choices=IMPACT_AREAS)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='planned',)
+
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+class ImpactRecord(models.Model):
+    project = models.ForeignKey(
+        ImpactProject,
+        on_delete=models.CASCADE,
+        related_name = "impact_records",
+    )
+
+    metric = models.CharField(max_length=100)
+    value = models.FloatField()
+    unit = models.CharField(max_length=50)
+
+    recorded_at = models.DateField()
+
+    def __str__(self):
+        return f"{self.metric} - {self.value} {self.unit}"
+    
